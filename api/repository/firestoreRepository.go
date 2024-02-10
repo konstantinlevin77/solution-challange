e1de3c5e6ceae9971471260eb7a112650f3334b4
Mehmet <tekmanmehmetyasar@protonmail.com>
2024-02-10 23:33:00 +0300

    Implement repository method GetAllUsers()

--- api/repository/firestoreRepository.go
+++ api/repository/firestoreRepository.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"github.com/konstantinlevin77/solution-challenge/api/driver"
 	"github.com/konstantinlevin77/solution-challenge/api/models"
+	"google.golang.org/api/iterator"
 )
 
 func NewFirestoreRepository() (*FirestoreRepository, error) {
@@ -23,4 +24,25 @@ func (fr *FirestoreRepository) AddUser(u models.User) error {
 
 	_, _, err := fr.Client.Collection("users").Add(context.Background(), u)
 	return err
+}
+
+func (fr *FirestoreRepository) GetAllUsers() ([]models.User, error) {
+
+	userList := make([]models.User, 0)
+	iter := fr.Client.Collection("users").Documents(context.Background())
+	for {
+
+		var u models.User
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return []models.User{}, err
+		}
+		_ = doc.DataTo(&u)
+		userList = append(userList, u)
+	}
+	return userList, nil
+
 }
