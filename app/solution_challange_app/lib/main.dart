3dcdf801da178c54196257ea4ecc08eb72a91c33
Mehmet <tekmanmehmetyasar@protonmail.com>
2024-02-20 00:31:27 +0300

    Implement secure storage service to later store the JWT token and the id of the users.

--- app/solution_challange_app/lib/main.dart
+++ app/solution_challange_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() async {
   runApp(const MainApp());
 }
---
+++ app/solution_challange_app/lib/src/services/storage_service.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorageService {
+  final FlutterSecureStorage storage = const FlutterSecureStorage();
+
+  Future<void> writeSecureData(String key, String value) async {
+    await storage.write(key: key, value: value);
+  }
+
+  Future<String> readSecureData(String key) async {
+    String value = await storage.read(key: key) ?? '';
+    return value;
+  }
+
+  Future<void> deleteSecureData(String key) async {
+    await storage.delete(key: key);
+  }
+}
