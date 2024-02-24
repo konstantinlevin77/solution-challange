68e05900c5782ead66b487a7b0533882430c7cd0
Mehmet <tekmanmehmetyasar@protonmail.com>
2024-02-24 10:28:06 +0300

    Small changes in API.

--- api/models/menuModel.go
+++ api/models/menuModel.go
@@ -6,11 +6,10 @@ type Menu struct {
 	ID               string       `json:"id" firestore:"id,omitempty"`
 	Name             string       `json:"name" firestore:"name,omitempty"`
 	BusinessId       string       `json:"business_id" firestore:"business_id,omitempty"`
+	BusinessName     string       `json:"business_name" firestore:"business_name,omitempty"`
 	Ingredients      []Ingredient `json:"ingredients" firestore:"ingredients,omitempty"`
 	Price            float64      `json:"price" firestore:"price,omitempty"`
 	ExampleImagePath string       `json:"example_image_path" firestore:"example_image_path,omitempty"`
-	AvgStars         float64      `json:"avg_stars" firestore:"avg_stars,omitempty"`
-	NumReviews       int          `json:"num_reviews" firestore:"num_reviews,omitempty"`
 	IsGlutenFree     bool         `json:"is_gluten_free" firestore:"is_gluten_free,omitempty"`
 	CreatedAt        time.Time    `json:"created_at" firestore:"created_at,omitempty"`
 	UpdatedAt        time.Time    `json:"updated_at" firestore:"updated_at,omitempty"`
